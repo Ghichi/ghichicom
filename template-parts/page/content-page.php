5508fef0a25506f1edac7ff4653f63d05cdcb6bf
渡邉　義一 <ghichi.wb@gmail.com>
2018-02-27 22:44:37 +0900

    Advanced Custom Fields Pro を使って、カスタム・フィールドを固定ページに適応。レイアウト配置の変更を図る。「繰り返し機能」を使って、コンテンツのテキストと画像を交互に表示させるプログラムを書いてみる。

--- template-parts/page/content-page.php
+++ template-parts/page/content-page.php
@@ -15,24 +15,51 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-<?php
-
-	/*
-	 * If a regular post or page, and not the front page, show the featured image.
-	 * Using get_queried_object_id() here since the $post global may not be set before a call to the_post().
-	 */
-	if ( ( is_single() || ( is_page() && ! twentyseventeen_is_frontpage() ) ) && has_post_thumbnail( get_queried_object_id() ) ) :
-		echo '<div class="single-featured-image-header">';
-		echo get_the_post_thumbnail( get_queried_object_id(), 'twentyseventeen-featured-image' );
-		echo '</div><!-- .single-featured-image-header -->';
-	endif;
-	?>
-
-
-		<?php twentyseventeen_edit_link( get_the_ID() ); ?>
+		<div class="entry-sub-title"><?php the_field('page-subtitle'); ?></div>
+		<div class="entry-header-2">
+			<div class="entry-lead" ><?php the_field('page-lead'); ?></div>		
+			<?php
+				/*
+				 * アイキャッチ画像が入ります。
+				 */
+				if ( ( is_single() || ( is_page() && ! twentyseventeen_is_frontpage() ) ) && has_post_thumbnail( get_queried_object_id() ) ) :
+					echo '<div class="single-featured-image-header">';
+					echo get_the_post_thumbnail( get_queried_object_id(), 'twentyseventeen-featured-image' );
+					echo '</div><!-- .single-featured-image-header -->';
+				endif;
+				?>
+		</div><!-- .entry-header-2 -->
+<?php twentyseventeen_edit_link( get_the_ID() ); ?>
 	</header><!-- .entry-header -->
 	<div class="entry-content">
+
+<?php
+// コンテンツ
+if( have_rows('page-section') ):
+ 	// loop through the rows of data
+    while ( have_rows('page-section') ) : the_row(); ?>
+<section class="<?php        // 位置を決める
+		the_sub_field('location'); ?>">
+	<div class="entry-content-first">
+	<?php        // 初めのコンテンツ（テキスト）
+		the_sub_field('entry-content-first'); ?>	
+	</div>
+	<div class="entry-content-second">
+		<?php        // 次のコンテンツ（画像）
+		the_sub_field('entry-content-second'); ?>
+		</div>
+</section>
+
+<?php endwhile;
+else :
+    // no rows found
+endif;
+?>
+
+
+
+
+		
 		<?php
 			the_content();
 
